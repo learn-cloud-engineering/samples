2a4573777ffd5cfc03b80cf4e6d568f3fe30b833
Jesstern Rays <jsstrn@users.noreply.github.com>
2025-12-17 04:47:15 +0800

    Remove default value and validate instance type

--- hello-world-ec2/variables.tf
+++ hello-world-ec2/variables.tf
@@ -1,16 +1,15 @@
 # You can change this to your preferred region
 variable "region" {
-  default     = "us-east-1"
   description = "The AWS region to deploy resources in"
   type        = string
 
   validation {
     condition = contains([
       "ap-southeast-1", # Singapore
-      "us-east-1",      # North Virginia
       "me-south-1",     # Bahrain
+      "us-east-1",      # North Virginia
     ], var.region)
-    error_message = "Error: Not a valid region."
+    error_message = "💁 Region must be either ap-southeast-1, me-south-1, or us-east-1."
   }
 }
 
@@ -18,4 +17,9 @@ variable "instance_type" {
   default     = "t3.micro"
   description = "Free tier eligible instance type"
   type        = string
+
+  validation {
+    condition = (var.instance_type == "t3.micro")
+    error_message = "value"
+  }
 }
